a244f6ffb919d04a2f74ebc66a9e909a6d0d4d99
jbfronda <frondajohnbryan@gmail.com>
2023-03-10 19:28:21 +0800

    Added route for user login

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -5,5 +5,7 @@ const userController = require("../Controllers/userController.js");
 // Routes without params
 // User registration
 router.post("/register", userController.userRegistration);
+// User authentication
+router.post("/login", userController.userAuthentication);
 
 module.exports = router;
