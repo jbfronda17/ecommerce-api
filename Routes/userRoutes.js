9dd7ed7da938796900549da525c20a99799e1a3e
jbfronda <frondajohnbryan@gmail.com>
2023-03-10 19:42:16 +0800

    Added route for user details

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/userController.js");
+const auth = require("../auth.js");
 
 // Routes without params
 // User registration
 router.post("/register", userController.userRegistration);
 // User authentication
 router.post("/login", userController.userAuthentication);
+// User details
+router.get("/details", auth.verify, userController.userDetails);
 
 module.exports = router;
