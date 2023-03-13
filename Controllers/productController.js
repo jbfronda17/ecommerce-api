212a11a41972990cfa039b2b0118fb4153615782
jbfronda <frondajohnbryan@gmail.com>
2023-03-13 11:15:08 +0800

    Added controller for adding product

---
+++ Controllers/productController.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Product = require("../Models/productSchema.js");
+const auth = require("../auth.js");
+
+// Create product (Admin only)
+module.exports.addProduct = (request, response) => {
+	let input = request.body;
+	const userData = auth.decode(request.headers.authorization);
+	if(userData.isAdmin === false){
+		return response.send("Access denied. Not an admin.")
+	}
+	else{
+		Product.findOne({name: input.name})
+		.then(result => {
+			if(result !== null){
+				return response.send("This product already exists!")
+			}
+			else{
+				let newProduct = new Product({
+					image: input.image,
+					name: input.name,
+					description: input.description,
+					stock: input.stock,
+					price: input.price
+				})
+				newProduct.save()
+				.then(product => response.send(product))
+				.catch(error => response.send(error));
+			}
+		})
+		.catch(error => response.send(error));
+	}
+};
