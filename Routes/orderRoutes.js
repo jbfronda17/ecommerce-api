60359c43395545b4b52dd3f8256e1f8b252e57ba
jbfronda <frondajohnbryan@gmail.com>
2023-03-15 13:54:40 +0800

    Added route for checkout order

--- Routes/orderRoutes.js
+++ Routes/orderRoutes.js
@@ -3,4 +3,8 @@ const router = express.Router();
 const auth = require("../auth.js");
 const orderController = require("../Controllers/orderController.js");
 
+// Routes with params
+// Non-admin user checkout (Create order)
+router.post("/:productId", auth.verify, orderController.createOrder);
+
 module.exports = router;
